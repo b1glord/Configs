8e8192673cbf7454e378fc04aacdacca83640b31
BigLorD <FuryTr@yandex.com>
2020-03-31 12:00:37 +0300

    Update libav.sh

--- youtube-dl/libav.sh
+++ youtube-dl/libav.sh
@@ -1,3 +1,5 @@
+# Centos 7 libav Kurulum
+## https://wiki.libav.org/Platform/Linux#Pre_Built_Static_Binaries
 yum -y install autoconf automake gcc gcc-c++ git libtool make yasm pkgconfig zlib-devel
 
 # Download lib av from https://www.libav.org/download/
