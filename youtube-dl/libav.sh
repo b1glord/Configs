89a0f9ccae87f67c592a5196a0c07a9aebf9c9c4
BigLorD <FuryTr@yandex.com>
2020-03-31 11:18:30 +0300

    Create libav.sh

---
+++ youtube-dl/libav.sh
@@ -0,0 +1,6 @@
+yum -y install autoconf automake gcc gcc-c++ git libtool make yasm pkgconfig zlib-devel
+
+# Download lib av from https://www.libav.org/download/
+cd /tmp
+wget -nc https://www.libav.org/releases/libav-12.3.tar.gz
+tar  -xvf libav-12.3.tar.gz
