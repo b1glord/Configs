a7748b74ae2851701e865f1c2d9f88c97cfd8b00
BigLorD <FuryTr@yandex.com>
2020-11-21 16:52:54 +0300

    Update installcentos.sh

--- NopCommerce/installcentos.sh
+++ NopCommerce/installcentos.sh
@@ -125,12 +125,20 @@ expect eof
 
 # Install nginx
 # Install the nginx package:
+# Add nginx Repo
+wget -N https://raw.githubusercontent.com/b1glord/ispconfig_setup_extra/master/centos7/nginx/nginx.repo -P /etc/yum.repos.d
+sudo yum-config-manager --enable nginx-mainline
+yum -y install nginx
+# add modules
+yum -y install nginx-module-geoip nginx-module-image-filter nginx-module-njs nginx-module-perl nginx-module-xslt
 
-sudo yum -y install nginx
-
-# Run the nginx service:
-sudo systemctl enable nginx
-sudo systemctl start nginx
+# Configure Nginx Default Port 81
+#sed -i "s/    listen       80;/    listen       81;/" /etc/nginx/conf.d/default.conf
+# Configure cgi.fix_pathinfo
+sed -i "s/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php.ini
+# Configure startup and start
+systemctl enable nginx.service
+systemctl start nginx.service
 
 # and check its status:
 sudo systemctl status nginx
