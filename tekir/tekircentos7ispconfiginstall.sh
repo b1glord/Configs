338f85e06931dca10e46769e64ce5108853478d7
BigLorD <FuryTr@yandex.com>
2021-01-27 15:17:27 +0300

    Update tekircentos7ispconfiginstall.sh

--- tekir/tekircentos7ispconfiginstall.sh
+++ tekir/tekircentos7ispconfiginstall.sh
@@ -1,7 +1,68 @@
 #https://www.tekir.com.tr/indir/tekir-linux-kurulum/
 
+read -p "Please enter your MySQL username: " username
+if [[ -z "$username" ]]; then
+        echo "ERROR: That username is invalid or you didn't enter a value."
+        exit
+        # If username is valid or field is not blank continue and ask for the password
+elif [[ -n "$username" ]]; then
+        read -sp "Please enter your MySQL Password: " password
+fi
 
-yum -y install wget curl python3-pip
+# This checks if the password is valid and the field is not blank.
+# Also checks if the confirmation password is valid and not blank.
+# Then checks and compares the two passwords.
+# If one password is typed incorrectly it will error out.
+if [[ -z "$password" ]]; then
+        echo ""
+        echo "ERROR: That password is invalid or you didn't enter a value."
+        exit
+        # Password is valid.
+elif [[ -n "$password" ]]; then
+        echo ""
+        read -sp "Please re-enter your MySQL Password: " password2
+        # Confirmation Password is invalid.
+        if [[ -z "$password2" ]]; then
+                echo ""
+                echo "ERROR: The second password entered was invalid or you didn't enter a value."
+                exit
+        fi
+fi
+# Password comparing
+if [[ -n "$password2" ]] && [[ "$password" == "$password2" ]]; then
+        echo ""
+        echo ""
+        echo "Passwords match continuing..."
+        echo ""
+        read -p "Please provide us your hostname for MySQL (default is localhost): " hostname
+
+        # Checks if Passwords do not match
+elif [[ -n "$password2" ]] && [[ "$password" != "$password2" ]]; then
+        echo ""
+        echo ""
+        echo "ERROR: Passwords do not match."
+        exit
+fi
+
+# Ask what hostname this can be executed locally, but localhost is default.
+if [[ -z "$hostname" ]]; then
+        echo ""
+        echo "ERROR: Please set a proper hostname such as localhost."
+        exit
+        # If hostname is not blank, we will continue.
+elif [[ -n "$hostname" ]]; then
+        read -p "Please enter your MySQL database: " database
+fi
+
+# If database field is blank, it will error out.
+if [[ -z "$database" ]]; then
+        echo ""
+        echo "ERROR: The database name is invalid or blank."
+        exit
+fi
+
+
+yum -y install wget curl python3-pip expect
 pip3 install b1glord.yadisk-direct
 
 
@@ -37,8 +98,27 @@ yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
 yum -y install Percona-Server-server-56 Percona-Server-client-56
 service mysql start
 chkconfig --levels 235 mysql on
-mysql_secure_installation
-
+SECURE_MYSQL=$(expect -c "
+set timeout 3
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+expect \"root password?\"
+send \"y\r\"
+expect \"New password:\"
+send \"$password\r\"
+expect \"Re-enter new password:\"
+send \"$password\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
 
 
 #utf8 çalışmasını sağlama almak için /etc dizinindeki my.cnf dosyasında ilgili bölümlere şu satırları ekleyelim:
