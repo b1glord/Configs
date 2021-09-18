e66cc5baaa98b5bac5138adb0e57a2fb12f697fa
BigLorD <FuryTr@yandex.com>
2021-09-18 19:41:42 +0300

    Update dynu.sh

--- centos/dynu/dynu.sh
+++ centos/dynu/dynu.sh
@@ -1,12 +1,11 @@
 #https://www.dynu.com/en-US/Resources/Downloads
 #Download Dynu Client for Red Hat Enterprise Linux 7
 rpm -ivh https://www.dynu.com/support/downloadfile/30
-
+#
 #Configure Dynu Client for Red Hat Enterprise Linux 7 (pvpgn.freeddns.org)
 sed -i "s/username/username aktifhost/" /etc/dynuiuc/dynuiuc.conf
 sed -i "s/location/location work/" /etc/dynuiuc/dynuiuc.conf
-
+#
 #Add Password Manuel
 yum -y install nano
 nano /etc/dynuiuc/dynuiuc.conf
-
