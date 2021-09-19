8fbad11bf97065158d5a2a92898fc2e9d76ebfb4
BigLorD <FuryTr@yandex.com>
2021-09-19 23:12:24 +0300

    Update dynu.sh

--- centos/dynu/dynu.sh
+++ centos/dynu/dynu.sh
@@ -1,6 +1,7 @@
 #https://www.dynu.com/en-US/Resources/Downloads
 #Download Dynu Client for Red Hat Enterprise Linux 7
-rpm -ivh https://www.dynu.com/support/downloadfile/30
+rpm -ivh https://github.com/b1glord/Configs/raw/master/centos/dynu/dynuiuc-2.6.2-2.el7.x86_64.rpm
+rpm -Uvh https://www.dynu.com/support/downloadfile/30
 #
 #Configure Dynu Client for Red Hat Enterprise Linux 7 (pvpgn.freeddns.org)
 sed -i "s/username/username aktifhost/" /etc/dynuiuc/dynuiuc.conf
