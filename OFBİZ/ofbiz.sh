21148c83ffe51ddd0d1dfdce3dcfa9c683c3304e
BigLorD <FuryTr@yandex.com>
2022-01-01 14:39:07 +0300

    Update ofbiz.sh

--- OFBİZ/ofbiz.sh
+++ OFBİZ/ofbiz.sh
@@ -1,38 +1,29 @@
-## apache ofbiz 
-
-## java 8 ile çalışıyor üst sürümlere uyumlu degil
-
+# apache ofbiz 
+# java 8 ile çalışıyor üst sürümlere uyumlu degil
 sudo yum -y install java-1.8.0-openjdk-devel
 sudo yum -y install java-1.8.0-openjdk
-
-
 javahomedir = $(dirname $(readlink $(readlink $(which java))))
 echo JAVA_HOME="$javahomedir" > /etc/profile.d/java.sh
 
-##source /etc/profile.d/java.sh
-##echo $JAVA_HOME
-
-##kontrol ediyoruz
+# source /etc/profile.d/java.sh
+# echo $JAVA_HOME
+# kontrol ediyoruz
 java –version
 
-
-## gradle kurulumu https://gradle.org/releases/
+# gradle kurulumu https://gradle.org/releases/
 cd /tmp
 wget https://services.gradle.org/distributions/gradle-7.3.3-all.zip
-
 sudo mkdir /opt/gradle
 sudo unzip -d /opt/gradle gradle-7.3.3-all.zip
 export PATH=$PATH:/opt/gradle/gradle-7.3.3/bin
-
-##kontrol ediyoruz
+# kontrol ediyoruz
 gradle –v
 
-
-
+# apache ofbiz i indiriyoruz
 cd /home/ofbiz/
 git clone https://github.com/apache/ofbiz.git 
 git clone https://gitbox.apache.org/repos/asf/ofbiz-plugins.git plugins 
-git pull
+# git pull
 
 ./gradlew cleanAll loadAll
 
