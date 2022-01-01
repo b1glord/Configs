70225acdb350b9791f7b1bc162fff367b91780fe
BigLorD <FuryTr@yandex.com>
2022-01-01 12:45:27 +0300

    Update ofbiz.sh

--- OFBİZ/ofbiz.sh
+++ OFBİZ/ofbiz.sh
@@ -1,6 +1,6 @@
-apache ofbiz 
+## apache ofbiz 
 
-// java 8 ile çalışıyor üst sürümlere uyumlu degil
+## java 8 ile çalışıyor üst sürümlere uyumlu degil
 
 sudo yum -y install java-1.8.0-openjdk-devel
 sudo yum -y install java-1.8.0-openjdk
@@ -9,14 +9,14 @@ sudo yum -y install java-1.8.0-openjdk
 $javahomedir = dirname $(readlink $(readlink $(which java)))
 echo JAVA_HOME="$javahomedir" > /etc/profile.d/java.sh
 
-//source /etc/profile.d/java.sh
-//echo $JAVA_HOME
+##source /etc/profile.d/java.sh
+##echo $JAVA_HOME
 
-//kontrol ediyoruz
+##kontrol ediyoruz
 java –version
 
 
-// gradle kurulumu https://gradle.org/releases/
+## gradle kurulumu https://gradle.org/releases/
 cd /tmp
 wget https://services.gradle.org/distributions/gradle-7.3.3-all.zip
 
@@ -24,7 +24,7 @@ sudo mkdir /opt/gradle
 sudo unzip -d /opt/gradle gradle-7.3.3-all.zip
 export PATH=$PATH:/opt/gradle/gradle-7.3.3/bin
 
-//kontrol ediyoruz
+##kontrol ediyoruz
 gradle –v
 
 
@@ -32,9 +32,10 @@ gradle –v
 cd /home/ofbiz/
 git clone https://github.com/apache/ofbiz.git 
 git clone https://gitbox.apache.org/repos/asf/ofbiz-plugins.git plugins 
-git clone https://gitbox.apache.org/repos/asf/ofbiz-framework.git ofbiz-framework 
 git pull
-
 
 ./gradlew cleanAll loadAll
 
+
+
+git clone https://gitbox.apache.org/repos/asf/ofbiz-framework.git ofbiz-framework 
