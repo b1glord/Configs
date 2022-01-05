bf22f182e11d4a2b88ec7d2fb178d310c20b28b3
BigLorD <FuryTr@yandex.com>
2022-01-05 09:50:00 +0300

    Update install_oraclejdk8.sh

--- OFBİZ/install_oraclejdk8.sh
+++ OFBİZ/install_oraclejdk8.sh
@@ -4,10 +4,10 @@
 java -version
 
 # Get the latest Oracle Java SDK http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
-wget -c --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn/java/jdk/8u311-b11/4d5417147a92418ea8b615e228bb6935/jdk-8u311-linux-x64.rpm" --output-document="jdk-8u31-linux-x64.rpm"
-
+# https://github.com/frekele/oracle-java
+wget https://github.com/frekele/oracle-java/releases/download/8u92-b14/jdk-8u92-linux-x64.rpm
 # Install Java SDK
-sudo rpm -i jdk-8u31-linux-x64.rpm
+sudo rpm -i jdk-8u92-linux-x64.rpm
 
 # Check if the default java version is set to sun jdk
 java -version
