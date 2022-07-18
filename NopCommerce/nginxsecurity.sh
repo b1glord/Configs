353d7fb1a8fb725ba33142ee49b7069035b84a22
BigLorD <FuryTr@yandex.com>
2022-07-18 12:03:11 +0300

    Create nginxsecurity.sh

---
+++ NopCommerce/nginxsecurity.sh
@@ -0,0 +1,16 @@
+# https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/linux-nginx?view=aspnetcore-6.0#configuring-supervisor
+#
+# Secure Nginx from clickjacking
+# https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/linux-nginx?view=aspnetcore-6.0#secure-nginx-from-clickjacking
+
+
+
+
+
+
+
+
+
+
+
+# https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/linux-nginx?view=aspnetcore-6.0#mime-type-sniffing
