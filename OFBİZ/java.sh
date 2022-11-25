351b778859bfa4870e686a5136f6f45938c7654f
BigLorD <FuryTr@yandex.com>
2022-11-25 10:29:09 +0300

    Create java.sh

---
+++ OFBİZ/java.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Set JDK installation directory according to selected Java compiler
+
+export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+export PATH=$PATH:$JAVA_HOME/bin
+export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
