d7a3a4470ddccb12317a5f66d79ed08a2eef506b
BigLorD <FuryTr@yandex.com>
2022-11-25 10:30:12 +0300

    Update and rename java.sh to javahome.sh

--- OFBİZ/javahome.sh
+++ OFBİZ/javahome.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Set JDK installation directory according to selected Java compiler
+# Copy /etc/profile.d location
 
 export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 export PATH=$PATH:$JAVA_HOME/bin
