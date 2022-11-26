a1d57150261e3a2a5e1e78f169e6abd979cabb73
BigLorD <FuryTr@yandex.com>
2022-11-26 12:12:59 +0300

    Update AdoptJDK8.sh

--- OFBİZ/AdoptJDK8.sh
+++ OFBİZ/AdoptJDK8.sh
@@ -12,3 +12,19 @@ EOF
 
 #yum -y install adoptopenjdk-8-openj9
 yum -y install adoptopenjdk-8-hotspot
+
+# Add Profile Java Home
+cat <<'EOF' >> /etc/profile.d/javahome.sh
+#!/bin/sh
+# Set JDK installation directory according to selected Java compiler
+# Copy /etc/profile.d location
+# Test Command 
+# echo $JAVA_HOME
+# echo $PATH
+# echo $CLASSPATH
+export JAVA_HOME=/usr/java/default
+export PATH=$PATH:$JAVA_HOME/bin
+export CLASSPATH=$JAVA_HOME/jre/lib/ext:$JAVA_HOME/lib/tools.jar
+# export JRE_HOME=$JAVA_HOME/jre
+EOF
+
