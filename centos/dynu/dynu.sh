055af97fb4b853daa597ace3a262483f184961f4
BigLorD <FuryTr@yandex.com>
2022-12-09 19:19:37 +0300

    Update dynu.sh

--- centos/dynu/dynu.sh
+++ centos/dynu/dynu.sh
@@ -1,12 +1,54 @@
+#!/bin/sh
+clear
+echo ""
+echo "Welcome to the DYNU installer"
+echo "This script will install DYNU."
+#echo ""
+#echo "This script was created by BigLorD "
+echo ""
+echo ""
+
+# Ask user fror the DYNU username
+# Then check if the username field is blank
+# if blank it will error out
+read -p "Please enter your MySQL username: " username
+if [[ -z "$username" ]]; then
+        echo "ERROR: That username is invalid or you didn't enter a value."
+        exit
+        # If username is valid or field is not blank continue and ask for the password
+elif [[ -n "$username" ]]; then
+        read -sp "Please enter your MySQL Password: " password
+fi
+
+# This checks if the password is valid and the field is not blank.
+# Also checks if the confirmation password is valid and not blank.
+# Then checks and compares the two passwords.
+# If one password is typed incorrectly it will error out.
+if [[ -z "$password" ]]; then
+        echo ""
+        echo "ERROR: That password is invalid or you didn't enter a value."
+        exit
+        # Password is valid.
+elif [[ -n "$password" ]]; then
+        echo ""
+        read -sp "Please re-enter your MySQL Password: " password2
+        # Confirmation Password is invalid.
+        if [[ -z "$password2" ]]; then
+                echo ""
+                echo "ERROR: The second password entered was invalid or you didn't enter a value."
+                exit
+        fi
+fi
 #https://www.dynu.com/en-US/Resources/Downloads
 #Download Dynu Client for Red Hat Enterprise Linux 7
 rpm -ivh https://github.com/b1glord/Configs/raw/master/centos/dynu/dynuiuc-2.6.2-2.el7.x86_64.rpm
 rpm -Uvh https://www.dynu.com/support/downloadfile/30
 #
 #Configure Dynu Client for Red Hat Enterprise Linux 7 (pvpgn.freeddns.org)
-sed -i "s/username/username aktifhost/" /etc/dynuiuc/dynuiuc.conf
+sed -i "s/username/username $username/" /etc/dynuiuc/dynuiuc.conf
 sed -i "s/location/location work/" /etc/dynuiuc/dynuiuc.conf
+sed -i "s/password/password $password/" /etc/dynuiuc/dynuiuc.conf
 #
 #Add Password Manuel
-yum -y install nano
-nano /etc/dynuiuc/dynuiuc.conf
+#yum -y install nano
+#nano /etc/dynuiuc/dynuiuc.conf
