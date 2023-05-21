179d525101438b0d249046142371c73d11c6371f
Fury <FuryTr@yandex.com>
2023-05-22 00:17:47 +0300

    Create certbot.sh

---
+++ OFBİZ/certbot.sh
@@ -0,0 +1,17 @@
+# https://certbot.eff.org/instructions?ws=nginx&os=pip
+
+# Install system dependencies
+sudo yum -y install python3 augeas-libs
+
+# Remove certbot-auto and any Certbot OS packages
+sudo yum -y remove certbot
+
+# Set up a Python virtual environment
+sudo python3 -m venv /opt/certbot/
+sudo /opt/certbot/bin/pip install --upgrade pip
+
+# Install Certbot
+sudo /opt/certbot/bin/pip install certbot certbot-nginx
+
+# Prepare the Certbot command
+sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
