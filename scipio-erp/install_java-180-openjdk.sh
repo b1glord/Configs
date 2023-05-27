1843feeffc1b61d11e42704dbb0896a27238fe26
Fury <FuryTr@yandex.com>
2023-05-27 20:23:03 +0300

    Create install_java-180-openjdk.sh

---
+++ scipio-erp/install_java-180-openjdk.sh
@@ -0,0 +1,21 @@
+yum install java-1.8.0-openjdk
+
+# update-alternatives --config java
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.372.b07-1.el7_9.x86_64/jre/bin/java
+
+# Add Profile Java Home
+cat <<'EOF' >> /etc/profile.d/javahome.sh
+#!/bin/sh
+# Set JDK installation directory according to selected Java compiler
+# Copy /etc/profile.d location
+# Test Command 
+# echo $JAVA_HOME
+# echo $PATH
+# echo $CLASSPATH
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.372.b07-1.el7_9.x86_64/jre
+export JRE_HOME=$JAVA_HOME/jre
+export PATH=$PATH:$JAVA_HOME/bin
+export CLASSPATH=$JAVA_HOME/jre/lib/ext:$JAVA_HOME/lib/tools.jar
+EOF
+
+source /etc/profile.d/javahome.sh
