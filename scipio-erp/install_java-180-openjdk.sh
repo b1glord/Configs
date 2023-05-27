66b335648a2524cb0305193f41267f0306d66166
Fury <FuryTr@yandex.com>
2023-05-27 20:31:48 +0300

    Update install_java-180-openjdk.sh

--- scipio-erp/install_java-180-openjdk.sh
+++ scipio-erp/install_java-180-openjdk.sh
@@ -1,4 +1,4 @@
-yum install java-1.8.0-openjdk
+yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
 
 # update-alternatives --config java
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.372.b07-1.el7_9.x86_64/jre/bin/java
@@ -12,7 +12,7 @@ cat <<'EOF' >> /etc/profile.d/javahome.sh
 # echo $JAVA_HOME
 # echo $PATH
 # echo $CLASSPATH
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.372.b07-1.el7_9.x86_64/jre
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.372.b07-1.el7_9.x86_64
 export JRE_HOME=$JAVA_HOME/jre
 export PATH=$PATH:$JAVA_HOME/bin
 export CLASSPATH=$JAVA_HOME/jre/lib/ext:$JAVA_HOME/lib/tools.jar
