197689541b34671698dc9416cffb5903e311435e
Fury <FuryTr@yandex.com>
2023-05-28 21:41:45 +0300

    Update scipio.sh

--- scipio-erp/scipio.sh
+++ scipio-erp/scipio.sh
@@ -1,3 +1,4 @@
+# https://www.scipioerp.com/community/developer/installation-configuration/
 # scipio erp Prerequisites
 wget -nc https://raw.githubusercontent.com/b1glord/Configs/master/scipio-erp/Prerequisites.sh -P /tmp
 chmod a+x /tmp/Prerequisites.sh
@@ -7,8 +8,9 @@ chmod a+x /tmp/Prerequisites.sh
 git clone https://github.com/ilscipio/scipio-erp.git /usr/local/scipio-erp
 
 cd /usr/local/scipio-erp
+git checkout scipioce-1.14
 
-git checkout 2.x
+#git checkout 2.x
 
 # solr 8080 default port degistirme cakısmaları onlemek için
 # sed -i "s/solr.webapp.portOverride=/solr.webapp.portOverride=2480/" /usr/local/scipio-erp/applications/solr/config/solrconfig.properties
