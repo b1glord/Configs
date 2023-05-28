e0d2d5464871e4867db0eaa0319a272608415b6c
Fury <FuryTr@yandex.com>
2023-05-28 10:35:56 +0300

    Update scipio.sh

--- scipio-erp/scipio.sh
+++ scipio-erp/scipio.sh
@@ -1,3 +1,10 @@
+# scipio erp
+# java 1.8 ile çalışıyor üst sürümlere uyumlu degil
+cd /tmp
+wget https://raw.githubusercontent.com/b1glord/Configs/master/scipio-erp/install_java-180-openjdk.sh
+chmod +x install_java-180-openjdk.sh
+./install_java-180-openjdk.sh
+
 yum -y install ant
 
 git clone https://github.com/ilscipio/scipio-erp.git /usr/local/scipio-erp
