b696e0307c8f4cec2078d253c65d98cc51d27b1c
Fury <FuryTr@yandex.com>
2023-05-28 21:39:14 +0300

    Update Prerequisites.sh

--- scipio-erp/Prerequisites.sh
+++ scipio-erp/Prerequisites.sh
@@ -3,10 +3,16 @@ wget -nc https://raw.githubusercontent.com/b1glord/ispconfig_setup_extra/master/
 chmod +x /tmp/install_github.sh
 /tmp/install_github.sh
 
-# java 1.8 ile çalışıyor üst sürümlere uyumlu degil
-wget -nc https://raw.githubusercontent.com/b1glord/Configs/master/scipio-erp/install_java-180-openjdk.sh -P /tmp
-chmod +x /tmp/install_java-180-openjdk.sh
-/tmp/install_java-180-openjdk.sh
+## java 1.8 ile çalışıyor üst sürümlere uyumlu degil
+# wget -nc https://raw.githubusercontent.com/b1glord/Configs/master/scipio-erp/install_java-180-openjdk.sh -P /tmp
+# chmod +x /tmp/install_java-180-openjdk.sh
+# /tmp/install_java-180-openjdk.sh
+
+# java 8 ile çalışıyor üst sürümlere uyumlu degil
+cd /tmp
+wget https://raw.githubusercontent.com/b1glord/Configs/master/OFBİZ/install_oraclejdk8.sh
+chmod +x install_oraclejdk8.sh
+./install_oraclejdk8.sh
 
 # ant
 yum -y install ant
