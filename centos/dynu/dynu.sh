570e6c23ac6548321a2ced350097fc4e8011ca80
Fury <FuryTr@yandex.com>
2023-12-25 20:30:38 +0300

    Update dynu.sh

--- centos/dynu/dynu.sh
+++ centos/dynu/dynu.sh
@@ -5,7 +5,7 @@
 #rpm -Uvh https://github.com/b1glord/Configs/raw/master/centos/dynu/dynuiuc-2.6.2-2.el7.x86_64.rpm
 cd /tmp
 #curl https://www.dynu.com/Support/DownloadFile/30
-curl https://github.com/b1glord/Configs/raw/master/centos/dynu/dynuiuc-2.6.2-2.el7.x86_64.rpm
+curl -O https://github.com/b1glord/Configs/raw/master/centos/dynu/dynuiuc-2.6.2-2.el7.x86_64.rpm
 rpm -Uvh dynuiuc-2.6.2-2.el7.x86_64.rpm
 # start scprits
 #
