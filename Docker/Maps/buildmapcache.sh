0a98b191a2da81d948e72d518dc4178910028447
Fury <FuryTr@yandex.com>
2025-10-21 19:14:32 +0300

    Create buildmapcache.sh

---
+++ Docker/Maps/buildmapcache.sh
@@ -0,0 +1,19 @@
+# 1) Derleme araclari (root isen sudo gerekmez)
+apt update && apt install -y cmake build-essential zlib1g-dev
+
+# 2) Temiz build klasoru
+cd /opt/rathena
+rm -rf build && mkdir build && cd build
+
+# 3) Cikis dizinlerini tools/ olarak ayarla ve projeyi hazirla
+cmake \
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=/opt/rathena/tools \
+  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=/opt/rathena/tools \
+  -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=/opt/rathena/tools \
+  ..
+
+# 4) Sadece mapcache hedefini derle
+cmake --build . --target mapcache --config Release -j
+
+# 5) Kontrol
+ls -l /opt/rathena/tools/mapcache
