3ec8bbbc80e5e81bac7f5b2168bf92f29507137b
Fury <FuryTr@yandex.com>
2025-10-22 15:07:35 +0300

    Update buildmapcache.sh

--- Docker/Maps/buildmapcache.sh
+++ Docker/Maps/buildmapcache.sh
@@ -1,8 +1,8 @@
 # 1) Derleme araclari (root isen sudo gerekmez)
-apt update && apt install -y cmake build-essential zlib1g-dev
+# apt update && apt install -y cmake build-essential zlib1g-dev
 
 # 2) Temiz build klasoru
-cd /opt/rathena
+cd /opt/rathena/src/tool
 rm -rf build && mkdir build && cd build
 
 # 3) Cikis dizinlerini tools/ olarak ayarla ve projeyi hazirla
